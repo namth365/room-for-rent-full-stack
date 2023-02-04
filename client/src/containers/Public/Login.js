854c8e129dbfdff5a223fc5dbd496685c2e91f7d
Your Name <namth365@gmail.com>
2023-02-04 18:59:58 +0700

    Create Register

--- client/src/containers/Public/Login.js
+++ client/src/containers/Public/Login.js
@@ -1,13 +1,27 @@
 import React, { useEffect, useState } from 'react'
 import { Button, InputForm } from '../../components'
 import { useLocation } from 'react-router-dom'
+import { apiRegister } from '../../services/auth'
+import * as actions from '../../store/actions'
+import {useDispatch} from 'react-redux'
 
 function Login() {
   const location = useLocation()
+  const dispatch = useDispatch()
   const [isRegister, setIsRegister] = useState(location.state?.flag)
+  const [payload, setPayload] = useState({
+    name: '',
+    phone: '',
+    password: ''
+  })
   useEffect(() => {
     setIsRegister(location.state?.flag)
   }, [location.state?.flag])
+  
+  const handleSubmit = async () => {
+    dispatch(actions.register(payload))    
+  }
+  
   
   return (
     <div className='bg-white w-[600px] p-[30px] pb-[100px] rounded-md shadow-sm border border-[#DEDEDE]'>
@@ -15,14 +29,15 @@ function Login() {
         {isRegister ? 'Tạo tài khoản mới' : 'Đăng Nhập'}
       </h3>
       <div className='w-full flex flex-col gap-4'>
-        {isRegister && <InputForm label={'HỌ TÊN'} />}
-        <InputForm label={'SỐ ĐIỆN THOẠI'} />
-        <InputForm label={'MẬT KHẨU'} />
+        {isRegister && <InputForm label={'HỌ TÊN'} value={payload.name} setValue={setPayload} type={'name'} />}
+        <InputForm label={'SỐ ĐIỆN THOẠI'} value={payload.phone} setValue={setPayload} type={'phone'} />
+        <InputForm label={'MẬT KHẨU'} value={payload.password} setValue={setPayload} type={'password'} />
         <Button
-          text='Đăng nhập'
+          text= {isRegister ? 'Dang ki' : 'Đăng nhập' } 
           bgColor='bg-secondary1'
           textColor='text-white'
           fullWidth
+          onClick={handleSubmit}
         />
       </div>
       <div className='mt-7 flex items-center justify-between'>
@@ -31,7 +46,8 @@ function Login() {
             className='text-[blue] hover:text-orange cursor-pointer '> Đăng nhập ngay </span>
         </small> : <>
           <small className='text-[blue] hover:text-orange cursor-pointer '>Bạn quên mật khẩu ?</small>
-          <small onClick={() => { setIsRegister(true) }} className='text-[blue] hover:text-orange cursor-pointer '>Tạo tài khoản mới</small>
+          <small onClick={() => { setIsRegister(true) }} className='text-[blue] hover:text-orange cursor-pointer '>
+            Tạo tài khoản mới</small>
         </>}
       </div>
     </div>
