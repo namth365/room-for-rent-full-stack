187c5b6b12457022994aaa026043c289a401a38e
Your Name <namth365@gmail.com>
2023-02-05 19:11:56 +0700

    Create Register Login Logout

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -4,13 +4,25 @@ const instance = axios.create( {
     baseURL: process.env.REACT_APP_SERVER_URL
 })
 
-instance.interceptors.request.use(function(config) {
-    const token = localStorage.getItem('persist:auth')
-    console.log(token)
+// Add a request interceptor
+instance.interceptors.request.use(function (config) {
+    // Do something before request is sent
     return config;
-}, function (error) {
-    console.log(error);
-    return Promise.reject(error)
+  }, function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  });
+
+// Add a response interceptor
+instance.interceptors.response.use(function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    // Refresh Token
+    return response;
+  }, function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
   });
 
 export default instance
